bd373bb45bd92e54acf6486ab4c0d2fae43ac61e
Robert Leverington <robert.leverington@roh.org.uk>
2016-01-28 13:59:12 +0000

    - Update dependencies: phpunit/phpunit to 5.1.*, seld/jsonlint to 1.4.*, sensio/buzz-bundle to 1.0.*, sensio/distribution-bundle to 5.0.*, sensio/generator-bundle to 3.0.*, fabpot/php-cs-fixer to 1.11.*.
    - Disable allowing dev dependencies as sensio/buzz-bundle has now been tagged.
    
    - Update Symfony application files to meet current standards.
    
    - Add code coverage whitelist configuration to PHPUnit, required since version 5.

--- app/autoload.php
+++ app/autoload.php
@@ -1,9 +1,13 @@
 <?php
 
 use Doctrine\Common\Annotations\AnnotationRegistry;
+use Composer\Autoload\ClassLoader;
 
-$loader = require __DIR__.'/../vendor/autoload.php';
+$loader = require __DIR__ . '/../vendor/autoload.php';
 
+/**
+ * @var ClassLoader $loader
+ */
 AnnotationRegistry::registerLoader([$loader, 'loadClass']);
 
 return $loader;
